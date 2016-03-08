d548f194b14cd8f674394a4942ac6a9939046fe9
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-08 15:26:40 +0900

    Move main class

--- src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/MapeAutoscalingAnalysisApplication.java
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/MapeAutoscalingAnalysisApplication.java
@@ -1,4 +1,4 @@
-package jp.ac.nii.prl;
+package jp.ac.nii.prl.mape.autoscaling.analysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/jp/ac/nii/prl/MapeAutoscalingAnalysisApplicationTests.java
+++ src/test/java/jp/ac/nii/prl/MapeAutoscalingAnalysisApplicationTests.java
@@ -3,6 +3,9 @@ package jp.ac.nii.prl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import jp.ac.nii.prl.mape.autoscaling.analysis.MapeAutoscalingAnalysisApplication;
+
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
