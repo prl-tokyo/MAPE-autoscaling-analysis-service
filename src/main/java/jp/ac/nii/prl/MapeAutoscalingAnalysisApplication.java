9ae01f353f88fbb94e8743f40dd28ab011d81412
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-08 14:27:37 +0900

    Initial import

---
+++ src/main/java/jp/ac/nii/prl/MapeAutoscalingAnalysisApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeAutoscalingAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeAutoscalingAnalysisApplication.class, args);
+	}
+}
