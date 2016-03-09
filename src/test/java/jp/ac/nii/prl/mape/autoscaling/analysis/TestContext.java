71b68cd728ba760b4cf76daddfe48f1eed5dda60
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-09 17:41:49 +0900

    Add VirtualMachineController mock to testing context

--- src/test/java/jp/ac/nii/prl/mape/autoscaling/analysis/TestContext.java
+++ src/test/java/jp/ac/nii/prl/mape/autoscaling/analysis/TestContext.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import jp.ac.nii.prl.mape.autoscaling.analysis.service.DeploymentService;
+import jp.ac.nii.prl.mape.autoscaling.analysis.service.VirtualMachineService;
 
 @Configuration
 public class TestContext {
@@ -12,5 +13,10 @@ public class TestContext {
 	@Bean
 	public DeploymentService deploymentService() {
 		return Mockito.mock(DeploymentService.class);
+	}
+	
+	@Bean
+	public VirtualMachineService virtualMachineService() {
+		return Mockito.mock(VirtualMachineService.class);
 	}
 }
