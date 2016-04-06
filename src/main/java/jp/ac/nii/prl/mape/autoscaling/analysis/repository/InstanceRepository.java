5a30ce985b01765e3de6b7b7065d8b2194c69228
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-06 17:12:22 +0900

    Rename VirtualMachineRepository to InstanceRepository

---
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/repository/InstanceRepository.java
@@ -0,0 +1,13 @@
+package jp.ac.nii.prl.mape.autoscaling.analysis.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.ac.nii.prl.mape.autoscaling.analysis.model.Instance;
+
+public interface InstanceRepository extends JpaRepository<Instance, Integer> {
+
+	Collection<Instance> findByDeploymentId(Integer deploymentId);
+
+}
