47169af5825dcbbe0b34568a27540ded1178d0b1
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-26 09:32:00 +0900

    Validation constraints on configuration properties

--- src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/configuration/AnalysisProperties.java
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/configuration/AnalysisProperties.java
@@ -1,5 +1,11 @@
 package jp.ac.nii.prl.mape.autoscaling.analysis.configuration;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -7,10 +13,20 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("app.analysis")
 public class AnalysisProperties {
 
+	@Valid
+	@NotNull
+	@Min(0)
+	@Max(1)
 	private Double maxThreshold;
 	
+	@Valid
+	@NotNull
+	@Min(0)
+	@Max(1)
 	private Double minThreshold;
 	
+	@Valid
+	@NotEmpty
 	private String securityGroup;
 
 	public Double getMaxThreshold() {
