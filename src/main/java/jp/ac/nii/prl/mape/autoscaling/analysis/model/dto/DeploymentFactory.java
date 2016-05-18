24f6c8bf74afaa22539b0b7c1b6c7e7892ade7b8
Lionel Montrieux <lionel@nii.ac.jp>
2016-05-18 18:22:07 -0500

    Skeleton factory methods

---
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/model/dto/DeploymentFactory.java
@@ -0,0 +1,42 @@
+package jp.ac.nii.prl.mape.autoscaling.analysis.model.dto;
+
+import jp.ac.nii.prl.mape.autoscaling.analysis.model.Adaptation;
+import jp.ac.nii.prl.mape.autoscaling.analysis.model.Deployment;
+import jp.ac.nii.prl.mape.autoscaling.analysis.model.Instance;
+import jp.ac.nii.prl.mape.autoscaling.analysis.model.InstanceType;
+
+public class DeploymentFactory {
+
+	public static Deployment createDeployment(DeploymentDTO dto) {
+		return null;
+	}
+	
+	public static DeploymentDTO createDeploymentDTO(Deployment deployment) {
+		return null;
+	}
+	
+	public static Adaptation createAdaptation(AdaptationDTO dto) {
+		return null;
+	}
+	
+	public static AdaptationDTO createAdaptationDTO(Adaptation adaptation) {
+		return null;
+	}
+	
+	public static Instance createInstance(InstanceDTO dto) {
+		return null;
+	}
+	
+	public static InstanceDTO createInstanceDTO(Instance instance) {
+		return null;
+	}
+	
+	public static InstanceType createInstanceType(InstanceTypeDTO dto) {
+		return null;
+	}
+	
+	public static InstanceTypeDTO createInstanceTypeDTO(InstanceType instanceType) {
+		return null;
+	}
+
+}
