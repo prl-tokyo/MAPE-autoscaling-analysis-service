a3f99791a349a91a99d32de2fa110960c4c9bc07
Lionel Montrieux <lionel@nii.ac.jp>
2016-05-19 13:07:30 -0500

    Implement Deployment to DeploymenDTO and back

--- src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/model/dto/DeploymentFactory.java
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/analysis/model/dto/DeploymentFactory.java
@@ -1,6 +1,12 @@
 package jp.ac.nii.prl.mape.autoscaling.analysis.model.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.LinkedMultiValueMap;
 
 import jp.ac.nii.prl.mape.autoscaling.analysis.model.Adaptation;
 import jp.ac.nii.prl.mape.autoscaling.analysis.model.Deployment;
@@ -8,13 +14,71 @@ import jp.ac.nii.prl.mape.autoscaling.analysis.model.Instance;
 import jp.ac.nii.prl.mape.autoscaling.analysis.model.InstanceType;
 
 public class DeploymentFactory {
+	
+	private final static Logger logger = LoggerFactory.getLogger(DeploymentFactory.class);
 
 	public static Deployment createDeployment(DeploymentDTO dto) {
-		return null;
+		Deployment deployment = new Deployment();
+		
+		deployment.setAdaptation(createAdaptation(dto.getAdaptation(), deployment));
+		
+		LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+		List<Instance> instances = new ArrayList<>();
+		for (InstanceDTO instanceDTO:dto.getInstances()) {
+			map.add(instanceDTO.getInstType(), instanceDTO.getInstID());
+			instances.add(createInstance(instanceDTO, deployment, null));
+		}
+		deployment.setInstances(instances);
+		
+		List<InstanceType> instanceTypes = new ArrayList<>();
+		for (InstanceTypeDTO instanceTypeDTO:dto.getInstanceTypes()) {
+			List<Instance> instancesOfType = new ArrayList<>();
+			for (String instID:map.get(instanceTypeDTO.getTypeID())) {
+				Optional<Instance> instance = deployment.getInstanceByInstID(instID);
+				if (instance.isPresent())
+					instancesOfType.add(instance.get());
+				else {
+					logger.error(String.format("Can't find instance with ID %s", instID));
+					throw new RuntimeException(String.format("Can't find instance with ID %s", 
+							instID));
+				}
+			}
+			instanceTypes.add(createInstanceType(instanceTypeDTO, deployment, instancesOfType));
+		}
+		deployment.setInstanceTypes(instanceTypes);
+		
+		for (Instance instance:deployment.getInstances()) {
+			Optional<InstanceType> instanceType = deployment.
+					getInstanceTypeByInstType(instance.getInstType());
+			if (instanceType.isPresent())
+				instance.setInstanceType(instanceType.get());
+			else {
+				logger.error(String.format("Can't find instance type with ID %s",
+						instance.getInstType()));
+				throw new RuntimeException(String.format("Can't find instance type with ID %s", 
+						instance.getInstType()));
+			}
+		}
+		
+		return deployment;
 	}
 	
 	public static DeploymentDTO createDeploymentDTO(Deployment deployment) {
-		return null;
+		DeploymentDTO dto = new DeploymentDTO();
+		
+		dto.setAdaptation(createAdaptationDTO(deployment.getAdaptation()));
+		
+		List<InstanceDTO> instanceDTOs = new ArrayList<>();
+		for (Instance instance:deployment.getInstances())
+			instanceDTOs.add(createInstanceDTO(instance));
+		dto.setInstances(instanceDTOs);
+		
+		List<InstanceTypeDTO> instanceTypeDTOs = new ArrayList<>();
+		for (InstanceType instanceType:deployment.getInstanceTypes())
+			instanceTypeDTOs.add(createInstanceTypeDTO(instanceType));
+		dto.setInstanceTypes(instanceTypeDTOs);
+		
+		return dto;
 	}
 	
 	public static Adaptation createAdaptation(AdaptationDTO dto, Deployment deployment) {
